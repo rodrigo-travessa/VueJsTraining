6eb2bb7c469bd20ef4cef177098f6069c81425da
Rodrigo Travessa <rodrigo.travessa@valtech.com>
2023-03-07 20:48:19 -0300

    template relocated inside HTML

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const app = Vue.createApp({
     //data, functions to react to shit
-    template :'<h2> I am the template <h2>'
+    //template :'<h2> I am the template <h2>'
 })
 
 app.mount('#app')
