e88e1c28e22011124291a6d2cc78388771935455
Rodrigo Travessa <rodrigo.travessa@valtech.com>
2023-03-07 20:57:11 -0300

    adding function that add 1 to likes on click

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const app = Vue.createApp({
     //data, functions to react to shit
     //template :'<h2> I am the template <h2>'
+    data(){
+        return {
+            likes : 50
+        }
+    }
 })
 
 app.mount('#app')
