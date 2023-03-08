9583d62537a315b6d4214d92b67f1425365d93c6
Rodrigo Travessa <rodrigo.travessa@valtech.com>
2023-03-07 21:37:22 -0300

    toggling on and off the showing of the number and changing the button text

--- phonyPhone.js
+++ phonyPhone.js
@@ -1,10 +1,18 @@
 const phonyPhone = Vue.createApp({
    data(){
     return {
-        phoneNumber : 0
+        phoneNumber : 0,
+        showNumber : false
 
     }
+   },
+   methods: {
+    toggleShowNumber(){
+        this.showNumber = !this.showNumber
+        console.log(this.showNumber)
+    }
    }
+
 })
 
 phonyPhone.mount('#phonyPhone')
