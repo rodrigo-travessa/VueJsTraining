b88e57d4fe01969d2bc2d5c6b08a876c0a27ab32
Rodrigo Travessa <rodrigo.travessa@valtech.com>
2023-03-07 21:06:01 -0300

    Calling function inside methods object, keyword This.prop to work

--- app.js
+++ app.js
@@ -3,8 +3,16 @@ const app = Vue.createApp({
     //template :'<h2> I am the template <h2>'
     data(){
         return {
-            likes : 50
+            likes : 50,
+            name : 'Rodrigo'
         }
+    },
+    methods: {
+        multiplyLikes(){
+            this.name = 'thiago'
+            this.likes = this.likes * 10
+        }
+
     }
 })
 
