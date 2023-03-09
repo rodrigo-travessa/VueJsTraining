a08277feeca175b22d7fe6fe1cc3e8a279c33e98
Rodrigo Travessa <rodrigo.travessa@valtech.com>
2023-03-09 16:11:09 -0300

    Added favorite Functionality by clicking on the book, it dinamicaly changes the card class and includes fav

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ const app = Vue.createApp({
             name : 'Rodrigo',
             x: 0,
             y: 0,
-            titles : [ 'title1', 'title2', 'title3', 'title4', 'title5', 'title6', 'title6'],
-            complex :[{title: 'name of the song', author: 'that guy', img:'assets/1.jpeg'},
-                      {title: 'name of the movie', author: 'this guy', img:'assets/2.jpeg'},
-                      {title: 'name of the play', author: 'those guys', img:'assets/3.jpeg'},
-                      {title: 'name of the stuff', author: 'them guys', img:'assets/4.jpeg'},
+            titles : [ 'title1', 'title2', 'title3', 'title4', 'title5', 'title6', 'title7'],
+            complex :[{title: 'name of the song', author: 'that guy', img:'assets/1.jpeg', isFav : true},
+                      {title: 'name of the movie', author: 'this guy', img:'assets/2.jpeg', isFav : true},
+                      {title: 'name of the play', author: 'those guys', img:'assets/3.jpeg', isFav : false},
+                      {title: 'name of the stuff', author: 'them guys', img:'assets/4.jpeg', isFav : true},
         ]}
     },
     methods: {
@@ -27,7 +27,10 @@ const app = Vue.createApp({
         handleMouseMove(e){
             this.x = e.offsetX,
             this.y = e.offsetY
-        }
+        },
+        toggleFav(item){
+            this.complex[this.complex.indexOf(item)].isFav = !item.isFav
+        },
 
     }
 })
